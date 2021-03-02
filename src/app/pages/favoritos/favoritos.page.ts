cc4143060a945744330fda91e0efae761f3ef2bf
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2021-03-02 17:11:49 +0100

    Limpiando código

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -33,42 +33,4 @@ export class FavoritosPage implements OnInit {
 
   ngOnInit() {}
 
-  /*deleteFromFavoritos(serie: Serie) {
-    this.service.deleteFromFavoritos(serie).then(
-      () => this.service.getFavoritos().then(
-        data => this.favoritos = data == null ? [] : data
-      )
-    );
-    this.clicked = !this.clicked;
-  }*/
-
-  /*public deleteSerieById(id: string): Promise<void> {
-    return this.db.collection('users/' + this.userId + '/series').doc(id).delete();
-  }*/
-
- /* public getSerieById(id: string): Observable<Serie> {
-    return this.db.collection('users/' + this.userId + '/series').doc<Serie>(id).valueChanges();
-  }*/
-
-  /*async alertDeleteFavorito(id: string, titulo: string) {
-    const alert = await this.alert.create({
-      header: 'Borrar serie',
-      message: `¿Estás seguro que quieres borrar la serie?`,
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          cssClass: 'secondary'
-        }, {
-          text: 'Quitar',
-          cssClass: 'danger',
-          handler: () => {
-            this.deleteSerieById(id);
-          }
-        }
-      ]
-    });
-    await alert.present();
-  }*/
-
 }
